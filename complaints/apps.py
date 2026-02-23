e31ae0437f87afa3af6e2e04c54668eb5398be9d
MehulWaghare <126313845+MehulWaghare@users.noreply.github.com>
2026-02-23 19:22:30 +0530

    Auto create default categories

--- complaints/apps.py
+++ complaints/apps.py
@@ -4,3 +4,24 @@ from django.apps import AppConfig
 class ComplaintsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'complaints'
+
+    def ready(self):
+        from django.db.utils import OperationalError
+        try:
+            from .models import Category
+
+            default_categories = [
+                "Water Problem",
+                "Road Damage",
+                "Electricity Issue",
+                "Garbage Issue",
+                "Drainage Problem",
+                "Street Light Not Working",
+            ]
+
+            for cat in default_categories:
+                Category.objects.get_or_create(name=cat)
+
+        except OperationalError:
+            # database not ready during migrations
+            pass
